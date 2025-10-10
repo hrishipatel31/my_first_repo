c781f99eeec4ae83d5920756c65a5d3b0469907f
hrishipatel31 <hrishipatel31@gmail.com>
2025-10-10 10:04:38 -0400

    added i2c code for stm32

---
+++ IIC/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/stm32demo/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/stm32demo/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/stm32demo/CMSIS/Include/core_cm4.h \
+ C:/stm32demo/CMSIS/Include/cmsis_version.h \
+ C:/stm32demo/CMSIS/Include/cmsis_compiler.h \
+ C:/stm32demo/CMSIS/Include/cmsis_gcc.h \
+ C:/stm32demo/CMSIS/Include/mpu_armv7.h \
+ C:/stm32demo/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+C:/stm32demo/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/stm32demo/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/stm32demo/CMSIS/Include/core_cm4.h:
+C:/stm32demo/CMSIS/Include/cmsis_version.h:
+C:/stm32demo/CMSIS/Include/cmsis_compiler.h:
+C:/stm32demo/CMSIS/Include/cmsis_gcc.h:
+C:/stm32demo/CMSIS/Include/mpu_armv7.h:
+C:/stm32demo/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
